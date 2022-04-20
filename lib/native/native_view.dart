8bae5b1e1a949bc38fb7fd08d1b2e115f6a49e74
Walker Zhao <ce.zhao@i-school.net>
2022-04-20 14:41:30 +0800

    添加原生View控制器，内部封装MethodChannel

---
+++ lib/native/native_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+///
+/// 原生视图包装组件
+///
+/// Author: walker
+/// Email: zhaocework@gmail.com
+/// Date: 2022/4/20
+class NativeViewWidget extends StatelessWidget {
+  const NativeViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
---
+++ lib/native/native_view_controller.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+///
+/// 负责与原生View通信的控制器
+///
+/// Author: walker
+/// Email: zhaocework@gmail.com
+/// Date: 2022/4/20
+class NativeViewController {
+  static const _kChannelNamePrefix = "walker.flutter.view_channel";
+  static const _kMethodSendToNative = "sendToNative";
+  static const _kMethodReceiveFromNative = "receiveFromNative";
+  static const _kParamMessage = "message";
+
+  // 负责通信的通道
+  late final MethodChannel _methodChannel;
+  // 接收消息后更新Provider
+  final Reader _reader;
+
+  NativeViewController(int viewId, this._reader) {
+    _methodChannel = MethodChannel('$_kChannelNamePrefix/$viewId');
+    _methodChannel.setMethodCallHandler(_handleMethod);
+  }
+
+  Future<dynamic> _handleMethod(MethodCall call) async {
+    switch (call.method) {
+      case _kMethodReceiveFromNative:
+        // 读取参数后，更新Provider
+        final message =
+            (call.arguments as Map<String, dynamic>)[_kParamMessage];
+        _reader(nativeMessageProvider.notifier).state = message;
+        return Future.value(0);
+    }
+  }
+
+  Future<void> sendToNative(String text) async {
+    try {
+      final result = await _methodChannel
+          .invokeMethod(_kMethodSendToNative, {_kParamMessage: text});
+      if (kDebugMode) {
+        print('Result for sendToNative: $result');
+      }
+    } on PlatformException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    } on MissingPluginException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+}
+
+// 从平台端发送来的消息
+final nativeMessageProvider = StateProvider<String>((ref) {
+  return "";
+});
+
+// TODO 原生View通信控制器
+final nativeViewControllerProvider =
+    StateProvider<NativeViewController?>((ref) {
+  return null;
+});
